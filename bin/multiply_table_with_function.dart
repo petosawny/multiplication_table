3db996689ee649816a52985919304eefa9494f2c
petosawny <93099971+petosawny@users.noreply.github.com>
2022-12-15 04:45:03 +0200

    Update multiply_table_with_function.dart

--- bin/multiply_table_with_function.dart
+++ bin/multiply_table_with_function.dart
@@ -1,24 +1,26 @@
 
+// import packages and classes
 import 'dart:io';
 
 void main(List<String> arguments) {
-
+// calling the method multiplicationTable defined below the main
 multiplicationTable();
 
 }
 
-
+// void fun create the multiplication table
 multiplicationTable (){
-  print("enter the number ");
+  print("enter the number ");   //print statement to the user
 
-  int? number = int.parse(stdin.readLineSync()!);
-
+  int? number = int.parse(stdin.readLineSync()!);   // insert value to the variable number  from user
+// first for to pass throw every number from start number to the final number in the sequence " the master number of the multiplication table "
   for (int i = number; i <= number    ; i++) {
+    // second for to  pass throw every number from start number to the final number in the sequence which is 12 in this sequence "the second number  of the multiplication table "
     for (int j = 1; j <= 12; j++) {
-      int result = i * j;
-      print(''' -> $i  *  $j    ='''  "\t" + result.toString());
+      int result = i * j;   // define a variable with initialization with an equation
+      print(''' -> $i  *  $j    ='''  "\t" + result.toString());    // print the them of the calculation equation to the user
     }
-    print("---------------");
+    print("---------------");     // symbols refers to the end of the program
   }
 }
 
